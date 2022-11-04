c1a2b5c2ba96f57d175ed473567aab5e7edc061e
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-11-04 12:40:58 -0400

    grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   document.title = 'GW Class';
   return (
-    <div className="container">
-      <div></div>
-      <header className="App-header">
-      </header>
+    <div className="container-fluid h-100 border border-info">
+      <div className='row h-25 border border-danger'>Title</div>
+      <div className='row h-100 border border-success'>
+        <div className='col-sm-3 border border-danger'>Lesson Column</div>
+        <div className='col-sm-9 border border-danger'>Text Column</div>
+      </div>
     </div>
   );
 }
