54c21c44c3936ffe68ad4f40bf70a5da07f9ad33
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-11-04 12:19:13 -0400

    new repo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  document.title = 'GW Class';
   return (
-    <div className="App">
+    <div className="container">
+      <div></div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
